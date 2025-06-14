3795743940784bbd7e61b6665ebfa26644e06829
Sokunneath <ss6024010025@camtech.edu.kh>
2025-06-14 09:32:24 +0700

    update

--- Receipt.java
+++ Receipt.java
@@ -1,19 +1,19 @@
 import java.security.Timestamp;
-import java.util.List;
+
 
 public class Receipt {
-    public int receiptID;
-    public int orderID;
-    public int tableNumber;
-    public List<Order> items;
-    public double totalPrice;
-    public Timestamp dateTime;
+    private int receiptID;
+    private int orderID;
+    private int tableNumber;
+    private int foodID;
+    private double totalPrice;
+    private Timestamp dateTime;
 
     public Receipt(int receiptID, Order order) {
         this.receiptID = receiptID;
         this.orderID = orderID;
         this.tableNumber = tableNumber;
-        this.items = items;
+        this.foodID = foodID;
         this.totalPrice = totalPrice;
         this.dateTime = dateTime;
     }
